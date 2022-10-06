60ef9565d37bf8555fa47ac16756096da6eb2825
gremushka <42302302+gremushka@users.noreply.github.com>
2022-10-06 12:49:34 +0300

    Update 02_task_algo.py

--- Module10/practice/algo/02_task_algo.py
+++ Module10/practice/algo/02_task_algo.py
@@ -7,3 +7,27 @@
 
 # Дополнительно: сделайте так, чтобы можно было гарантированно угадать число за 7 попыток.
 
+def AskQuestion(start_range, end_range):
+    test_num = start_range+(end_range - start_range) // 2
+    print(f'Ваше число {test_num}?')
+    answer = int(input('Введите  1 - равно, 2 - больше, 3 - меньше: '))
+    if answer == 1:
+        start_range, end_range = -1, test_num
+    elif answer == 2:
+        start_range=test_num
+    elif answer == 3:
+        end_range = test_num
+    else:
+        print('Ваш ответ непонятен.')
+
+    return [start_range, end_range]
+
+
+start_range = 1
+end_range = 100
+work = True
+while work:
+    start_range, end_range =AskQuestion(start_range, end_range)
+    if start_range == -1:
+        print('Загаданное число: ', end_range)
+        break
